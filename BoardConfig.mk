19ccc8584f5c98fe59c84da6208ea624229f01ad
SALMANKHANPM <patanmohammadsalmankhan@gmail.com>
2021-05-23 13:40:34 +0000

    X00TD: remove BC for ZenParts
    Signed-off-by: SALMANKHANPM <patanmohammadsalmankhan@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,9 +7,6 @@
 # Inherit from sdm660-common
 include device/asus/sdm660-common/BoardConfigCommon.mk
 
-# Inherit from ZenParts
-include packages/apps/ZenParts/BoardConfigZen.mk
-
 DEVICE_PATH := device/asus/X00TD
 
 # Assert
