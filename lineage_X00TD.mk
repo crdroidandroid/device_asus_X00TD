6b6e638f07d311779094e6c84b61056923f322a2
dhimanparas20 <dhimanparas20@gmail.com>
2021-12-24 16:08:45 +0000

    X00TD: Redfin December FP
    Signed-off-by: dhimanparas20 <dhimanparas20@gmail.com>

--- lineage_X00TD.mk
+++ lineage_X00TD.mk
@@ -36,7 +36,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-asus
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=ASUS_X00T \
     PRODUCT_NAME=WW_X00T \
-    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.211001.001 7641976 release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 12 SQ1A.211205.008 7888514 release-keys"
 
 # Build fingerprint
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.211001.001/7641976:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:12/SQ1A.211205.008/7888514:user/release-keys"
